730fc789ac4c2b9ca6db0cfcb5a4056f1698f582
Rafael Mello <merorafael@gmail.com>
2021-06-24 09:30:24 -0300

    WeatherDaily component file converted to tsx(this component is not done yet)

---
+++ src/components/WeatherDaily/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const MainView = styled.View``
+
+const CommonText = styled.Text``
+
+const WeatherDaily: React.FC = () => {
+  return (
+    <MainView>
+      <CommonText>Daily list</CommonText>
+    </MainView>
+  )
+}
+
+export default WeatherDaily
